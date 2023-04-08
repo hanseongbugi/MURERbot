b385d99ae23fb22e63a6427dc02b3ffbcbdc9281
hanseongbugi <105718365+hanseongbugi@users.noreply.github.com>
2023-04-08 19:35:34 +0900

    toggle button

--- murerbot/src/components/SignUp.js
+++ murerbot/src/components/SignUp.js
@@ -26,32 +26,32 @@ const SignUp = ()=>{
     }
 
     return (
-        <div class="signup_div">
-            <div class="signup_title_div">
+        <div className="signup_div">
+            <div className="signup_title_div">
                 <h1>물어봇</h1>
             </div>
-                <div class="signup_label_div"> 
+                <div className="signup_label_div"> 
                     <label htmlFor='input_id'>아이디</label>
                 </div><br/>
-                <input class="signup_input" type='text' name='input_id' placeholder="아이디를 입력하세요" value={inputId} onChange={handleInputId}/><br/>
-                <div class="signup_label_div">
+                <input className="signup_input" type='text' name='input_id' placeholder="아이디를 입력하세요" value={inputId} onChange={handleInputId}/><br/>
+                <div className="signup_label_div">
                     <label htmlFor='input_pw'>비밀번호</label>
                 </div><br/>
-                <input class="signup_input" type='password' name='input_pw' placeholder="비밀번호를 입력하세요" value={inputPw} onChange={handleInputPw}/><br/>
-                <div class="signup_label_div">
+                <input className="signup_input" type='password' name='input_pw' placeholder="비밀번호를 입력하세요" value={inputPw} onChange={handleInputPw}/><br/>
+                <div className="signup_label_div">
                     <label htmlFor='input_pw'>비밀번호 확인</label>
                 </div><br/>
-                <input class="signup_input" type='password' name='reInput_pw' placeholder="비밀번호를 다시 입력하세요" value={reInputPw} onChange={handleReInputPw}/><br/>
-                <div class="signup_label_div">
+                <input className="signup_input" type='password' name='reInput_pw' placeholder="비밀번호를 다시 입력하세요" value={reInputPw} onChange={handleReInputPw}/><br/>
+                <div className="signup_label_div">
                     <label htmlFor='input_name'>이름</label>
                 </div><br/>
-                <input class="signup_input" type='text' name='input_name' placeholder="이름을 입력하세요" value={inputName} onChange={handleInputName}/><br/><br/>
+                <input className="signup_input" type='text' name='input_name' placeholder="이름을 입력하세요" value={inputName} onChange={handleInputName}/><br/><br/>
             <Link to="/">
-                <button class="signup_button" type='button' onClick={onClickSignUp}>회원가입</button>
+                <button className="signup_button" type='button' onClick={onClickSignUp}>회원가입</button>
             </Link>
-            <div class="goto_login_div">
-                <label class="goto_login_label">이미 계정이 있으신가요?</label>
-                <Link class="goto_login" to="/">로그인</Link>
+            <div className="goto_login_div">
+                <label className="goto_login_label">이미 계정이 있으신가요?</label>
+                <Link className="goto_login" to="/">로그인</Link>
             </div>
     </div>
     )
--- murerbot/src/components/menu/ChatMenu.js
+++ murerbot/src/components/menu/ChatMenu.js
@@ -1,46 +1,69 @@
-import React from "react";
+import React, {useState} from "react";
 import "../../css/menu/chatMenu.css"
 import {Icon} from '@iconify/react';
 import { Link } from 'react-router-dom'
+//import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const ChatMenu=()=>{
+    const [summaryOpen, setSummaryOpen] = useState(false)
+    const [comparisonOpen, setComparisonOpen] = useState(false)
+    const [recommandOpen, setRecommandOpen] = useState(false)
+    const isSumaryOpen=()=>{
+        if(summaryOpen) setSummaryOpen(false)
+        else setSummaryOpen(true)
+    }
+    const isComparisonOpen=()=>{
+        if(comparisonOpen) setComparisonOpen(false)
+        else setComparisonOpen(true)
+    }
+    const isRecommandOpen=()=>{
+        if(recommandOpen) setRecommandOpen(false)
+        else setRecommandOpen(true)
+    }
     return(
         <>
-            <div class="menu_title">물어봇</div>
-            <div class="menu_list">
-                <label class="menu" for="menu">요약</label>
-                <input id="menu" type="checkbox"/>
-                <nav id="main_nav">
-                    <ul>
-                        <li>html</li>
-                        <li>css</li>
-                        <li>javaScript</li>
-                    </ul>
-                </nav>
-                <label class="menu" for="menu">비교</label>
-                <input id="menu" type="checkbox"/>
-                <nav id="main_nav">
-                    <ul>
-                        <li>html</li>
-                        <li>css</li>
-                        <li>javaScript</li>
-                    </ul>
-                </nav>
-                <label class="menu" for="menu">추천</label>
-                <input id="menu" type="checkbox"/>
-                <nav id="main_nav">
-                    <ul>
-                        <li>html</li>
-                        <li>css</li>
-                        <li>javaScript</li>
-                    </ul>
-                </nav>
+            <div className="menu_title">물어봇</div>
+            
+            <div className="menu_list">
+                <div className="menu" onClick={isSumaryOpen}>
+                    <label>요약</label>
+                    <div className="menu_icon">
+                        {
+                        summaryOpen ?
+                        <Icon icon="material-symbols:arrow-drop-up-rounded" height={40}/>
+                        :<Icon icon="material-symbols:arrow-drop-down-rounded" height={40}/>
+                        }
+                    </div>
+                </div>
+                
+                <div className="menu" onClick={isComparisonOpen}>
+                    <label>비교</label>
+                    <div className="menu_icon">
+                    {
+                        comparisonOpen ?
+                        <Icon icon="material-symbols:arrow-drop-up-rounded" height={40}/>
+                        :<Icon icon="material-symbols:arrow-drop-down-rounded" height={40}/>
+                        }
+                    </div>
+                </div>
+                
+                <div className="menu" onClick={isRecommandOpen}>
+                    <label>추천</label>
+                    <div className="menu_icon">
+                    {
+                        recommandOpen ?
+                        <Icon icon="material-symbols:arrow-drop-up-rounded" height={40}/>
+                        :<Icon icon="material-symbols:arrow-drop-down-rounded" height={40}/>
+                        }
+                    </div>
+                </div>
+                
             </div>
             <hr/>
             <Link to="/">
-                <div class="logout_div">
-                    <Icon class="logout_icon" icon="ic:baseline-logout" color="white"/>
-                    <label class="logout_label">로그아웃</label>
+                <div className="logout_div">
+                    <Icon className="logout_icon" icon="ic:baseline-logout" color="white"/>
+                    <label className="logout_label">로그아웃</label>
                 </div>
             </Link>
         </>
