54cc43ee0678f13db3e4cb62bc5bb17886aee7ad
yuzzin <77273340+babo0121@users.noreply.github.com>
2023-04-16 00:12:08 +0900

    회원가입 아이디 유효성 검사

--- murerbot/src/components/SignUp.js
+++ murerbot/src/components/SignUp.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom'
+import axios from "axios";
 import "../css/signup.css"
+
+
+let state = "SUCCESS"
+let idPattern = /^[a-z0-9_-]{5,20}$/;
+
 
 const SignUp = ()=>{
     const [inputId, setInputId] = useState('')
+    var isDuplicated = false;
     const [inputPw, setInputPw] = useState('')
     const [reInputPw, setReInputPw] = useState('')
     const [inputName, setInputName] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
 
     const handleInputId = (e) => {
         setInputId(e.target.value)
@@ -21,7 +29,53 @@ const SignUp = ()=>{
         setInputName(e.target.value)
     }
 
-    const onClickSignUp = () => {
+
+    const checkIdDuplication = async (inputId) => {
+        try {
+            isDuplicated = {
+                "userId": inputId
+            }
+            
+            const res = await axios.post(
+                "/doubleCheckID",
+                isDuplicated
+              );
+            console.log(res.data);
+            state = res.data["state"]
+            if(state === "ID_POSSIBLE") {
+                isDuplicated = true;
+                setErrorMessage("이미 사용 중인 아이디입니다.");
+            } 
+            else if(state === "ID_IMPOSSIBLE"){
+                isDuplicated = false;
+            }
+            else {
+                console.log("double check id: SEND FAIL")
+            }
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!inputId) {
+            setErrorMessage("필수 정보입니다.")
+            return;
+        } else if (!idPattern.test(inputId)) {
+            setErrorMessage("5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.")
+            return;
+        }
+
+        await checkIdDuplication(inputId);
+    }
+    
+    const onClickSignUp = (e) => {
+        if (isDuplicated) {
+            e.preventDefault();
+        }
         console.log('click signup')
     }
 
@@ -32,9 +86,11 @@ const SignUp = ()=>{
                 <div className="signup_title_div">
                     <h1>물어봇</h1>
                 </div>
-                    <div className="signup_label_div"> 
-                        <label htmlFor='input_id'>아이디</label>
+                <form onSubmit={handleSubmit}>
+                <div className="signup_label_div"> 
+                    <label htmlFor='input_id'>아이디</label>
                     </div><br/>
+                    {isDuplicated && <p style={{color: "red" }}>{errorMessage}</p>}
                     <input className="signup_input" type='text' name='input_id' placeholder="아이디를 입력하세요" value={inputId} onChange={handleInputId}/><br/>
                     <div className="signup_label_div">
                         <label htmlFor='input_pw'>비밀번호</label>
@@ -48,9 +104,11 @@ const SignUp = ()=>{
                         <label htmlFor='input_name'>이름</label>
                     </div><br/>
                     <input className="signup_input" type='text' name='input_name' placeholder="이름을 입력하세요" value={inputName} onChange={handleInputName}/><br/><br/>
-                <Link to="/">
-                    <button className="signup_button" type='button' onClick={onClickSignUp}>회원가입</button>
-                </Link>
+                    <Link to="/" onClick={onClickSignUp}>
+                        <button type="submit" className="signup_button">회원가입</button>
+                    </Link>
+                </form>
+                   
                 <div className="goto_login_div">
                     <label className="goto_login_label">이미 계정이 있으신가요?</label>
                     <Link className="goto_login" to="/">로그인</Link>
