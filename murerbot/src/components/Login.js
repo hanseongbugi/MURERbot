c521bfb2934cace63ebb24172ae4ac04cfb09fbd
hanseongbugi <105718365+hanseongbugi@users.noreply.github.com>
2023-04-17 14:47:44 +0900

    로그인 회원가입 오류 메시지 수정

--- murerbot/src/components/Login.js
+++ murerbot/src/components/Login.js
@@ -9,10 +9,6 @@ let state = "SUCCESS"
 const Login=()=>{
     const [inputId, setInputId] = useState('')
     const [inputPw, setInputPw] = useState('')
-    const [isIDError,setIsIDError]=useState(false)
-    const [isPwError,setIsPwError]=useState(false);
-    const [IDerrorMessage, setIDErrorMessage] = useState('');
-    const [PWerrorMessage, setPwErrorMessage] = useState('');
     const navigate = useNavigate()
 
     const handleInputId = (e) => {
@@ -29,20 +25,15 @@ const Login=()=>{
             }
         
             const res = await axios.post(
-                "/login",
+                "/signInUser",
                 userInfo
             );
             console.log(res.data);
             state = res.data["state"]
-            if(state==="LOGIN_SUCCESS")
+            if(state==="SIGNIN_SUCCESS")
                 navigate("/Chat")
-            else if(state === "ID_NOT_FOUND"){
-                setIDErrorMessage("아이디를 찾을 수 없습니다.")
-                setIsIDError(true)
-            }
-            else if(state === "PW_NOT_EQUAL"){
-                setPwErrorMessage("비밀번호가 일치하지 않습니다.")
-                setIsPwError(true)
+            else if(state === "SIGNIN_FAIL"){
+                alert("로그인에 실패하였습니다.")
             }
         }catch (error) {
             console.error(error);
@@ -50,18 +41,7 @@ const Login=()=>{
     }
     const onClickLogin = () => {
         console.log('click login')
-        setIsIDError(false)
-        setIsPwError(false)
-        if(inputId.length===0){
-            setIDErrorMessage("아이디를 입력해주세요.")
-            setIsIDError(true)
-        }
-        if(inputPw.length===0){
-            setPwErrorMessage("비밀번호를 입력해주세요.")
-            setIsPwError(true)
-        }
-        //sendLogin({inputId,inputPw}) //로그인 기능 활성화 시 주석 해제
-        navigate("/Chat") //sendLogin 메소드 사용 시 주석 처리
+        sendLogin({inputId,inputPw}) //로그인 기능 활성화 시 주석 해제
     }
 
     return (
@@ -80,14 +60,12 @@ const Login=()=>{
                     <div className="row">
                         <div className="col-12">
                             <input className="input_id" type='text' name='input_id' placeholder="아이디를 입력하세요" value={inputId} onChange={handleInputId}/>
-                            {isIDError && <p className="error_message">{IDerrorMessage}</p>}
                         </div>
                         
                     </div>
                     <div className="row">
                         <div className="col-12">
                             <input className="input_pw" type='password' name='input_pw' placeholder="비밀번호를 입력하세요"value={inputPw} onChange={handleInputPw}/>
-                            {isPwError && <p className="error_message">{PWerrorMessage}</p>}
                         </div>
                     </div>
                 </div>
