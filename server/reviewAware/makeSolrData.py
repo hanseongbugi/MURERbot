f912ebc32f2e594bc12fc87bdab7ddedbe26c472
HyunaJo <69022662+HyunaJo@users.noreply.github.com>
2023-04-20 18:08:10 +0900

    makeSolrData.py using json

--- server/reviewAware/makeSolrData.py
+++ server/reviewAware/makeSolrData.py
@@ -7,58 +7,40 @@
 
 from sentence_transformers import SentenceTransformer
 import json
-import pandas as pd
+
 model = SentenceTransformer('jhgan/ko-sbert-multitask')
 
-# json file load
-with open('data/laptop.json', 'r', encoding='utf-8') as f:
-    file_data = json.load(f)
-# with open('data/keyboard.json', 'r', encoding='utf-8') as f:
-#     file_data = json.load(f)
-# with open('data/monitor.json', 'r', encoding='utf-8') as f:
-#     file_data = json.load(f)
-# with open('data/mouse.json', 'r', encoding='utf-8') as f:
-#     file_data = json.load(f)
+keyboardFilename = 'keyboard'
+laptopFilename = 'laptop'
+monitorFilename = 'monitor'
+mouseFilename = 'mouse'
+desktopFilename = 'desktop'
 
-file_df = pd.DataFrame(file_data)
-file_df = file_df.drop([file_df.columns[1], file_df.columns[2], file_df.columns[4]], axis=1)
-new_content = {'reviews': []}
-for content in file_df['content']:
-    for score in content:
-        if score['score'] == 5:  # 5점 1점 이외에 리뷰는 무시 한다.
-            reviews = score['reviews']
-            new_content['reviews'].append(reviews)
-file_new_content = pd.DataFrame(new_content, columns=['reviews'])
-file_df = file_df.drop([file_df.columns[1]], axis=1)
-file_df['reviews'] = file_new_content
-new_json = []
-product = 0
+# fileNames = [laptopFilename, desktopFilename, monitorFilename, keyboardFilename, mouseFilename]
+fileNames = [desktopFilename]
+folder = "E:/Hansung/2023_Capstone/data/"
 
-for idx in range(len(file_df)):
-    data = file_df.iloc[idx]
-    product_name = data['name']
-    print(product_name)
-    index = 0
-    for reviews in data['reviews']:
-        for sentence in reviews['sentences']:
-            # print(sentence)
-            if len(sentence) < 2:
-                continue
-            sentence_embeddings = model.encode(sentence)  # 임베딩 벡터을 얻는다.
-            new_json.append({"product_id": product, 'vector_id': index, 'sentence': sentence,
-                            'vector': sentence_embeddings.tolist()})
-            index += 1
-    product += 1
+for fileName in fileNames:
+    print("=======================" + fileName + "=======================")
+    originalFilePath = folder + "review/" + fileName + "_reviewWithModified2.json"  # 원본 json 파일 경로
+    outputFilePath = folder + "embedding/" + fileName + "_embedding.json"  # 원본 json 파일 경로
 
-with open('data/laptop_embedding.json', 'w', encoding='utf-8') as out:
-    json.dump(new_json, out, indent=4, ensure_ascii=False)
+    # json file load
+    with open(originalFilePath, 'r', encoding='utf-8') as f:
+        file_data = json.load(f)
 
-# with open('data/keyboard_embedding.json', 'w', encoding='utf-8') as out:
-#     json.dump(new_json, out, indent=4, ensure_ascii=False)
+    new_json = []
+    for idx in range(len(file_data)):
+        print(str(idx)+"/"+str(len(file_data)))
+        data = file_data[idx]
+        index = 0
+        sentence = data["modified_sentence"]
+        if len(sentence) < 2:
+            continue
+        sentence_embeddings = model.encode(sentence)
+        new_json.append({"product_id": data["product_id"], 'vector_id': index, 'sentence': sentence,
+                         'vector': sentence_embeddings.tolist()})
+        index += 1
 
-# with open('data/monitor_embedding.json', 'w', encoding='utf-8') as out:
-#     json.dump(new_json, out, indent=4, ensure_ascii=False)
-#
-# with open('data/mouse_embedding.json', 'w', encoding='utf-8') as out:
-#     json.dump(new_json, out, indent=4, ensure_ascii=False)
-
+    with open(outputFilePath, 'w', encoding='utf-8') as out:
+        json.dump(new_json, out, indent=4, ensure_ascii=False)
