1b33bddcdf4f19cfbae76a7516a7327c76749590
daneng4 <daneng4@naver.com>
2023-05-21 22:01:16 +0900

    log productName

--- server/config.py
+++ server/config.py
@@ -3,7 +3,7 @@ DATABASE = {
     "password":"root",
     #host":"192.168.0.6",
     #"host":"192.168.0.3",
-    "host" : "223.194.152.194",
+    "host" : "127.0.0.1",
     "port":3307,
     "database":"murerbot"
 }
