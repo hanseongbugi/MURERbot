0d34227d512bc77aa1a82f708e566bba1b1dd166
hanseongbugi <bhsung1@naver.com>
2023-05-31 18:52:00 +0900

    비밀번호 메시지 수정

--- murerbot/src/components/SignUp.js
+++ murerbot/src/components/SignUp.js
@@ -6,7 +6,7 @@ import "../css/signup.css"
 
 let state = "SUCCESS"
 let idPattern = /^[a-z0-9_-]{5,20}$/;
-let pwPattern = /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z]).*$/;
+let pwPattern = /^.*(?=^.{5,20}$)(?=.*\d)(?=.*[a-zA-Z]).*$/;
 
 const SignUp = ()=>{
     const [inputId, setInputId] = useState('');
@@ -88,7 +88,7 @@ const SignUp = ()=>{
             setIsPwError(true)
         }
         else if(!pwPattern.test(inputPw)){
-            setPWErrorMessage("8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+            setPWErrorMessage("5~20자 영문 소문자, 숫자를 사용하세요.")
             setIsPwError(true)
         }
     }
