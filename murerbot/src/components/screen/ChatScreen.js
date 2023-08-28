56f0067e41fdbae2d155c84e8b8617ee8499ead7
yuzzin <77273340+babo0121@users.noreply.github.com>
2023-08-28 15:45:27 +0900

    추천 메시지 상품 이미지 띄우기

--- murerbot/src/components/screen/ChatScreen.js
+++ murerbot/src/components/screen/ChatScreen.js
@@ -326,7 +326,7 @@ const ChatScreen = React.forwardRef(({userId, nickName, chatLog,  tempItems, sum
                     <div key={'div'+idx}>{
                         msg[5]===1?<RightChatBubble key={'right'+idx} message={msg[3]} autoScroll={autoScroll} setAutoScroll={setAutoScroll} scrollbarRef={scrollbarRef}/>:
                         <LeftChatBubble key={'left'+msg[0]} idx={msg[0]} autoScroll={autoScroll} setAutoScroll={setAutoScroll} scrollbarRef={scrollbarRef} userMessage={message[idx-1][3]} itemArray={selectItemArray(msg[2])}
-                        firstMessage={false} selectProductName={selectProductName} state={msg[2]===5?"REQUIRE_DETAIL":"SUCCESS"} imageUrls={msg[2]===5?msg[7]:null}
+                        firstMessage={false} selectProductName={selectProductName} state={msg[2]===5?"REQUIRE_DETAIL":"SUCCESS"} imageUrls={msg[2]===5 || msg[2] === 2? msg[7]:null}
                         category={msg[2]} message={msg[3]} userId={userId} openModal={msg[2]===1?openModal :null} isShake={shakeBubble.includes(msg[0])} shakeBubble={shakeBubble} 
                         setShakeBubble={setShakeBubble} productName={msg[6]} bookmarkAlramEvent={bookmarkAlramEvent} sendMessage={onClickSend}/>
                     }
