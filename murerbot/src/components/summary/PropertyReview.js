63758c60069f3f9b1a5b4ad30fc4c9a83442c39d
yuzzin <77273340+yuzzin0121@users.noreply.github.com>
2023-09-18 02:19:48 +0900

    요약본 전체/긍정/부정 기능 생성

--- murerbot/src/components/summary/PropertyReview.js
+++ murerbot/src/components/summary/PropertyReview.js
@@ -1,40 +1,72 @@
 import {FaUser, FaRegSadTear} from "react-icons/fa";
 import "../../css/summary/summaryBook.css";
-import React from "react";
+import React, {useState} from "react";
 
 const PropertyReview = ({name, positiveSummary, negativeSummary}) => {
+    const [selectedIndex, setSelectedIndex] = useState(0)
+    const emotionArray = ["전체", "긍정", "부정"]
+    const clickEmotionButton = (e,index) => {
+        e.preventDefault();
+        setSelectedIndex(index)
+    }
 
     return (
         <>
-        {positiveSummary[0]?
-        <><p className="positive_p"><strong>{`긍정`}</strong></p>
-        <div className="user_review">
-            <div className="user_positive"><FaUser className="faUser" size={20} color={"#ffffff"}/></div>
-            <div className="sentiment_box"><p dangerouslySetInnerHTML={{__html:positiveSummary[0]}}/></div>
-        </div>
-        </>:null}
-        {positiveSummary[1]?
-        <>
-        <div className="user_review">
-            <div className="user_positive"><FaUser className="faUser" size={20} color={"#ffffff"}/></div>
-            <div className="sentiment_box"><p dangerouslySetInnerHTML={{__html:positiveSummary[1]}}/></div>
-        </div>
-        </>:null}
+        {(positiveSummary[0] || positiveSummary[1] || negativeSummary[0] || negativeSummary[1]) ? 
+        <div className="description">
+            <p>※전체 긍정/부정 리뷰 중 <bold>대표 리뷰를 선정한 결과</bold>입니다.</p>
+            <div className="emotion_buttons">
+                {emotionArray.map((value, idx)=>idx!==emotionArray.length?
+                    <button key={idx} onClick={(e) => clickEmotionButton(e,idx)} 
+                        style={{backgroundColor: selectedIndex === idx ? '#252B48' : '#f5f5f5',
+                        color: selectedIndex === idx ? '#ffffff' : '#000000'}}
+                    >{value}</button>
+                :null)}
+            </div>
+            
+        </div>:null}
+        
 
-        {negativeSummary[0]?
-        <><p className="negative_p"><strong>{`부정`}</strong></p>
-        <div className="user_review">
-            <div className="user_negative"><FaUser className="faUser" size={20} color={"#ffffff"}/></div>
-            <div className="sentiment_box"><p dangerouslySetInnerHTML={{__html:negativeSummary[0]}}/></div>
-        </div>
-        </>:null}
-        {negativeSummary[1]?
+        {selectedIndex === 0 || selectedIndex === 1 ?
+            <>
+            {positiveSummary[0]?
+            <><p className="positive_p"><strong>{`긍정`}</strong></p>
+            <div className="user_review">
+                <div className="user_positive"><FaUser className="faUser" size={20} color={"#ffffff"}/></div>
+                <div className="sentiment_box"><p dangerouslySetInnerHTML={{__html:positiveSummary[0]}}/></div>
+            </div>
+            </>:null}
+            {positiveSummary[1]?
+            <>
+            <div className="user_review">
+                <div className="user_positive"><FaUser className="faUser" size={20} color={"#ffffff"}/></div>
+                <div className="sentiment_box"><p dangerouslySetInnerHTML={{__html:positiveSummary[1]}}/></div>
+            </div>
+            </>:null}
+            </>
+        :null}
+
+        {selectedIndex === 0 || selectedIndex === 2 ?
         <>
-        <div className="user_review">
-            <div className="user_negative"><FaUser className="faUser" size={20} color={"#ffffff"}/></div>
-            <div className="sentiment_box"><p dangerouslySetInnerHTML={{__html:negativeSummary[1]}}/></div>
-        </div>
-        </>:null}
+            {negativeSummary[0]?
+            <><p className="negative_p"><strong>{`부정`}</strong></p>
+            <div className="user_review">
+                <div className="user_negative"><FaUser className="faUser" size={20} color={"#ffffff"}/></div>
+                <div className="sentiment_box"><p dangerouslySetInnerHTML={{__html:negativeSummary[0]}}/></div>
+            </div>
+            </>:null}
+            {negativeSummary[1]?
+            <>
+            <div className="user_review">
+                <div className="user_negative"><FaUser className="faUser" size={20} color={"#ffffff"}/></div>
+                <div className="sentiment_box"><p dangerouslySetInnerHTML={{__html:negativeSummary[1]}}/></div>
+            </div>
+            </>:null}
+        </>
+        :null}
+        
+
+        
 
         {(positiveSummary[0] || positiveSummary[1] || negativeSummary[0] || negativeSummary[1]) ? null:
         <div className="undefined_summary">
